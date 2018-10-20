3c506460200732a738c9c24d42e0e72744dd35f9
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-20 14:58:22 -0500

    Delegate error handling to server/helpers/dbErrorHandler.

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -1,4 +1,5 @@
 const Invoice = require('../models/InvoiceModel');
+const getErrorMessage = require('../helpers/dbErrorHandler');
 
 
 // Create a new invoice
@@ -6,25 +7,21 @@ const addInvoice = (req, res) => {
   const invoice = new Invoice(req.body);
   invoice.save((err, result) => {
     if (err) {
-      return res.status(400).json({
-        err
-      });
+      return res.status(400).json(getErrorMessage(err));
     }
     res.status(200).json(result);
   });
 };
 
 // Get all invoices
-const getInvoices = (req, res) => {
+const getAllInvoices = (req, res) => {
   Invoice.find({}, (err, docs) => {
     if (err) {
-      return res.status(400).json({
-        err
-      });
+      return res.status(400).json(getErrorMessage(err));
     }
     res.status(200).json(docs);
   })
 }
 
 
-module.exports = { addInvoice, getInvoices };
+module.exports = { addInvoice, getAllInvoices };
