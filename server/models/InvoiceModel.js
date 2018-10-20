9f1261eb5b0b2a8b15cef40cae58cc966e672f41
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-19 19:15:24 -0500

    Migrate company-related fields to CompanyModel.js.

--- server/models/InvoiceModel.js
+++ server/models/InvoiceModel.js
@@ -3,42 +3,10 @@ const mongoose = require('mongoose');
 
 // Invoice schema
 const InvoiceSchema = new mongoose.Schema({
-  companyName: {
-    type: String,
-    trim: true,
-    required: 'A company name is required.'
-  },
-  companyStreet1: {
-    type: String,
-    trim: true,
-    required: 'A street address is required.'
-  },
-  companyStreet2: {
-    type: String,
-    trim: true
-  },
-  companyCity: {
-    type: String,
-    trim: true,
-    required: 'A city is required.'
-  },
-  companyState: {
-    type: String,
-    trim: true,
-    required: 'A state is required.'
-  },
-  companyZip: {
-    type: Number,
-    trim: true,
-    required: 'A zip code is required.'
-  },
-  companyPhone: {
-    type: Number,
-    trim: true
-  },
-  companyEmail: {
-    type: String,
-    trim: true
+  companyId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Company',
+    required: true
   }
 }, { timestamps: true });
 
