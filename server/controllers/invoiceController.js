437b4b7efbd78fbfe6463e4a6f31622339a7d2f9
Austin W Keeton <austin.w.keeton@gmail.com>
2018-10-23 11:39:09 -0500

    Add functions for getting, deleting, updating invoices by ID.

--- server/controllers/invoiceController.js
+++ server/controllers/invoiceController.js
@@ -8,7 +8,7 @@ const addInvoice = (req, res) => {
     if (err) {
       return res.status(400).json(err);
     }
-    res.status(200).json(result);
+    res.status(201).json(result);
   });
 };
 
@@ -19,8 +19,44 @@ const getAllInvoices = (req, res) => {
       return res.status(400).json(err);
     }
     res.status(200).json(docs);
-  })
+  });
+};
+
+// Get one invoice by ID
+const getInvoiceById = (req, res) => {
+  Invoice.findById(req.params.id, (err, docs) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    res.status(200).json(docs);
+  });
+};
+
+// Delete an invoice by ID
+const deleteInvoiceById = (req, res) => {
+  Invoice.findByIdAndDelete(req.params.id, (err, docs) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    res.status(200).json(docs);
+  });
+};
+
+// Update an invoice by ID
+const updateIvoiceById = (req, res) => {
+  Invoice.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, docs) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    res.status(200).json(docs);
+  });
 }
 
 
-module.exports = { addInvoice, getAllInvoices };
+module.exports = {
+  addInvoice,
+  deleteInvoiceById,
+  getAllInvoices,
+  getInvoiceById,
+  updateIvoiceById
+};
